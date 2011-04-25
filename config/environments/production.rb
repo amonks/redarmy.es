a763410420bc96aab94d8109e0fb6b940a10fece
ajm <ajm@andrewmonks.net>
2011-04-24 23:38:07 -0400

    tentative mailer fix for chachi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 AlteredBeast::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.default_url_options = { :host => 'redarmy.es' }
+
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
